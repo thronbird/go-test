ffbc895afa59066c3ce63b75628453d2a0d9f8e0
luli <luli@tsign.cn>
2020-09-17 20:14:21 +0800

    init

--- test/pointer.go
+++ test/pointer.go
@@ -15,20 +15,20 @@ type Counter struct {
 
 func main() {
 	m := map[string]*Counter{"aaa": {T0: 111, Count: 222}}
-	change(m)
-	fmt.Println(m["aaa"].T0)
-
-	//for i:=1;i<=30;i++{
-	//	go change(m)
-	//}
+	//change2(m)
 	//fmt.Println(m["aaa"].T0)
+
+	for i := 1; i <= 30; i++ {
+		go change2(m)
+	}
+	fmt.Println(m["aaa"].T0)
 }
 
 func change(mp map[string]*Counter) {
 	for _, cnt := range mp {
 		println(&cnt.mux)
-		cnt.mux.Lock()
-		defer cnt.mux.Unlock()
+		//cnt.mux.Lock()
+		//defer cnt.mux.Unlock()//报错，下一步将Counter换掉，里面的锁也会被换掉
 		println(&cnt.mux)
 		*cnt = Counter{
 			T0:    cnt.T0 + 3,
@@ -37,4 +37,15 @@ func change(mp map[string]*Counter) {
 		println(&cnt.mux)
 	}
 	fmt.Println(mp["aaa"].T0)
+}
+
+func change2(mp map[string]*Counter) {
+	for _, cnt := range mp {
+		cnt.mux.Lock()
+		defer cnt.mux.Unlock()
+
+		cnt.T0 = cnt.T0 + 3
+		cnt.Count = 0
+	}
+	//fmt.Println(mp["aaa"].T0)
 }
