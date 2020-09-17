a782e7cd8024314ebc9450a2eea2fd9eeb590b2b
luli <luli@tsign.cn>
2020-09-17 15:45:12 +0800

    init

--- test/array.go
+++ test/array.go
@@ -1,14 +1,26 @@
 package main
 
-func main() {
-	arr := [...]int{1, 2, 3, 5}
-	print(arr[3])
+import "fmt"
 
-	// 声明一个需要 8 MB 的数组
-	var array [1e6]int
-	// 不使用指针会栈上分配8m的内存，浪费
-	foo(array)
-	foo2(&array)
+type A struct {
+	t int
+}
+
+func main() {
+	//arr := [...]int{1, 2, 3, 5}
+	//print(arr[3])
+	//print(arr[len(arr) -1 ])
+	//
+	//// 声明一个需要 8 MB 的数组
+	//var array [1e6]int
+	//// 不使用指针会栈上分配8m的内存，浪费
+	//foo(array)
+	//foo2(&array)
+
+	x := []A{{t: 0}, {t: 1}, {t: 2}}
+	fmt.Println(x[0])
+	x = x[2:]
+	fmt.Println(x[0])
 }
 
 func foo(arr [1e6]int) {
--- test/time.go
+++ test/time.go
@@ -9,10 +9,16 @@ import (
 func main() {
 	//t := time.Now()
 	//print(t.Month())
-	i, err := strconv.ParseInt("1600229553", 10, 64)
+	i, err := strconv.ParseInt("1600236899", 10, 64)
 	if err != nil {
 		panic(err)
 	}
 	tm := time.Unix(i, 0)
 	fmt.Println(tm)
+
+	//j:= 9898932
+	//print(j/1000)
+
+	k := 872
+	print(k % 100)
 }
